17c420474df151fa4c3fa6c4dc07d7e3abbb4714
Renan Medina <renan@silvamedina.com.br>
2024-07-30 12:45:25 -0300

    chore: Migrate from inertia to livewire

--- app/Frontend/Controllers/HomeController.php
+++ app/Frontend/Controllers/HomeController.php
@@ -3,14 +3,10 @@
 namespace App\Frontend\Controllers;
 
 use App\Core\Http\Controllers\Controller;
-use Inertia\Inertia;
 
 class HomeController extends Controller
 {
     public function index() {
-        return Inertia::render('Home', [
-            'applicationVersion' =>  \Illuminate\Foundation\Application::VERSION,
-            'phpVersion' => PHP_VERSION
-        ]);
+        return view('frontend.pages.home');
     }
 }
